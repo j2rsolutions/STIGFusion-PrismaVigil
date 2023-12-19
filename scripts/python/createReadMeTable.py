9702048d02aeb0f6e6af13a3017567c4c4391dcc
Jonathan Spigler <jonathanspigler@172-0-1-40.lightspeed.brhmal.sbcglobal.net>
2023-12-19 12:35:57 -0500

    updated readme for apache_server_2.4_unix_server

--- scripts/python/createReadMeTable.py
+++ scripts/python/createReadMeTable.py
@@ -12,7 +12,7 @@ def csv_to_markdown_table(csv_file):
             if not all(col in fields for col in required_columns):
                 raise ValueError("CSV must have id, severity, title, and description columns.")
 
-            with open('readme.md', mode='w', encoding='utf-8') as mdfile:
+            with open('README.md', mode='w', encoding='utf-8') as mdfile:
                 # Additional columns with default values
                 additional_columns = ['Custom Runtime Rule', 'Status', 'Assigned To', 'Link to Issue']
                 mdfile.write('| ' + ' | '.join(required_columns + additional_columns) + ' |\n')
@@ -27,7 +27,7 @@ def csv_to_markdown_table(csv_file):
                     default_values = ['TBD', 'In Progress', 'Unassigned', '']
                     mdfile.write('| ' + ' | '.join([row[col] for col in required_columns] + default_values) + ' |\n')
 
-        print("Markdown table has been successfully written to readme.md")
+        print("Markdown table has been successfully written to README.md")
     except FileNotFoundError:
         print(f"Error: File '{csv_file}' not found.")
     except ValueError as e:
